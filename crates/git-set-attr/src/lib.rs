07ce467311ba0546a926652d580d0b75cd045cd8
Joseph D. Carpinelli <joseph.d.carpinelli@loopy.codes>
2026-02-12 21:30:11 -0500

    feat: update scaffolding for `git-set-attr`

--- crates/git-set-attr/src/lib.rs
+++ crates/git-set-attr/src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! Set gitattributes via patterns and key-value pairs.
+
+pub use git2::{Error, Repository};
+
+/// A trait which provides methods for settings attributes in a Git repository.
+pub trait SetAttr {
+    /// Set attributes in the repository's root `.gitattributes` file.
+    fn set_attr(&self, pattern: &str, attributes: &[&str]) -> Result<(), Error>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl SetAttr for Repository {
+    fn set_attr(&self, pattern: &str, attributes: &[&str]) -> Result<(), Error> {
+        todo!()
     }
 }
