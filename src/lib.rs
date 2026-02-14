3a4dc2bc143063aff4de9b01c70b589cfbf4068e
Joseph D. Carpinelli <joseph.d.carpinelli@loopy.codes>
2026-02-14 10:08:06 -0500

    feat: add `Vendor` trait to `git-vendor`
    This trait currently has no implementations, and serves as rough
    scaffolding for how developers might use it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! Repositories are a myth!
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use git2::Error;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub trait Vendor {
+    /// Add the pattern to the appropriate `.gitattributes` file using `git_set_attr`.
+    ///
+    /// If there is a `.gitattributes` file in the current directory, that file is used.
+    /// Otherwise, the first found `.gitattributes` file when walking up the directory
+    /// tree from the current directory to the repository root directory is used.
+    ///
+    /// If the pattern is already specified, the `url` and `branch` are updated if necessary.
+    fn track_pattern(
+        &self,
+        pattern: &str,
+        url: &str,
+        maybe_branch: Option<&str>,
+    ) -> Result<(), Error>;
+
+    /// Remove the pattern from the appropriate `.gitattributes` file using `git_set_attr`.
+    ///
+    /// If there is a `.gitattributes` file in the current directory, that file is used.
+    /// Otherwise, the first found `.gitattributes` file when walking up the directory
+    /// tree from the current directory to the repository root directory is used.
+    fn untrack_pattern(&self, pattern: &str) -> Result<(), Error>;
+
+    /// Return the status of all vendored content, or any errors encountered along the way.
+    fn status(&self, maybe_pattern: Option<&str>) -> Result<(), Error>;
+
+    /// Fetch the latest content from all relevant vendor sources.
+    ///
+    /// All vendor refs are stored under `/refs/vendor/`.
+    fn fetch(&self, maybe_pattern: Option<&str>) -> Result<(), Error>;
+
+    /// Merge the latest content from all relevant vendor sources.
+    fn merge(&self, maybe_pattern: Option<&str>) -> Result<(), Error>;
 }
